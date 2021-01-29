b1ff2e6e5f15d0647b2a3bbdbd64f28e3cb06a9b
skyblaster89 <skyblaster89@gmail.com>
2021-01-29 11:48:03 -0600

    Infotech

---
+++ data.js
@@ -0,0 +1,61 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-infotech",
+      "name": "InfoTech",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1520,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": -2.425678154516584,
+          "pitch": 0.13556815200176509,
+          "title": "InfoTech Intro",
+          "text": ""
+        },
+        {
+          "yaw": -3.0442974522889834,
+          "pitch": 0.047963274007237544,
+          "title": "InfoTech 1",
+          "text": "Text"
+        },
+        {
+          "yaw": -2.772926358436669,
+          "pitch": 0.1141935557521272,
+          "title": "InfoTech 2",
+          "text": "Text"
+        }
+      ]
+    }
+  ],
+  "name": "InfoTech",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
